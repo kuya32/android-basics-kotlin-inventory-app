0580d246c2be49184b02a9cbdfefccd7848cdae7
Marchael Acode <m.acode@outlook.com>
2022-05-17 19:52:36 -0700

    Implemented item DAO and created multiple functions to access room database information.

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,4 +1,23 @@
 package com.example.inventory.data
 
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertItem(item: Item)
+
+    @Update
+    suspend fun updateItem(item: Item)
+
+    @Delete
+    suspend fun deleteItem(item: Item)
+
+    @Query("SELECT * from item WHERE id = :id")
+    suspend fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * from item ORDER BY name ASC")
+    fun getItems(): Flow<List<Item>>
 }
